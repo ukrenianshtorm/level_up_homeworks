967f2b7fc007265e87e927c6312ff914fa86443e
ukrenianshtorm <ukrenianshtorm@gmail.com>
2018-11-16 12:23:55 +0200

    js-6

--- public/views/javascript/homework-6/js/script.js
+++ public/views/javascript/homework-6/js/script.js
@@ -68,7 +68,9 @@
 // let str = 'java Script';
 
 // function ucFirst (str) {
-//   if (!str) return str;
+//   if (!str) {
+//     return str;
+//   }
 
 //   return str.charAt(0)
 //     .toUpperCase() + str.slice(1);
